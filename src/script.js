69df9c53fe4eb7ffbee0e87b9440755164157a2a
Comet300 <122388825+Comet300@users.noreply.github.com>
2024-10-07 08:33:52 +0300

    feat(ghosts,-lights-tweak): add moving ghosts, tweak door normal map to correct odd door lighting from pointlight

--- src/script.js
+++ src/script.js
@@ -257,10 +257,11 @@ const door = new THREE.Mesh(
     metalnessMap: textures.doorMetalness,
     roughnessMap: textures.doorRoughness,
     normalMap: textures.doorNormal,
+    normalScale: new THREE.Vector2(0.5, 0.5),
     transparent: true,
     alphaMap: textures.doorAlpha,
     displacementMap: textures.doorDisplacement,
-    displacementScale: 0.1,
+    displacementScale: 0.01,
   })
 );
 door.position.y = -0.2;
@@ -365,6 +366,14 @@ const doorLight = new THREE.PointLight("#ff7d46", 5);
 doorLight.position.set(0, 0.8, 2.3);
 house.add(doorLight);
 
+/**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight("#8800ff", 6);
+const ghost2 = new THREE.PointLight("#ff0088", 6);
+const ghost3 = new THREE.PointLight("#ff0000", 6);
+scene.add(ghost1, ghost2, ghost3);
+
 // Camera
 // -------
 
@@ -392,6 +401,31 @@ const tick = () => {
   // Timer
   timer.update();
   const elapsedTime = timer.getElapsed();
+
+  // Ghost animations
+  const ghost1Angle = elapsedTime * 0.5;
+  ghost1.position.x = Math.cos(ghost1Angle) * 4;
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y =
+    Math.sin(ghost1Angle) *
+    Math.sin(ghost1Angle * 2.34) *
+    Math.sin(ghost1Angle * 3.45);
+
+  const ghost2Angle = -elapsedTime * 0.38;
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y =
+    Math.sin(ghost2Angle) *
+    Math.sin(ghost2Angle * 2.34) *
+    Math.sin(ghost2Angle * 3.45);
+
+  const ghost3Angle = elapsedTime * 0.23;
+  ghost3.position.x = Math.cos(ghost3Angle) * 6;
+  ghost3.position.z = Math.sin(ghost3Angle) * 6;
+  ghost3.position.y =
+    Math.sin(ghost3Angle) *
+    Math.sin(ghost3Angle * 2.34) *
+    Math.sin(ghost3Angle * 3.45);
 
   // Update controls
   controls.update();
